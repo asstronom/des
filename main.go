7718a3c94fa202cfd8ba4b8f29dddf037926a964
@pudgebooster <ukraniandollar@gmail.com>
2022-10-24 16:16:45 +0300

    feature: make cipher out of key

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "flag"
+import (
+	"crypto/cipher"
+	"crypto/des"
+	"flag"
+	"fmt"
+	"log"
+)
 
 var isDecrypt bool
 var isTriple bool
@@ -9,6 +15,8 @@ var outputFile string
 var key string
 
 func main() {
+	var err error
+
 	flag.BoolVar(&isTriple, "triple", false, "sets encryption method to TripleDES")
 	flag.BoolVar(&isDecrypt, "decrypt", false, "sets mode to decrypt")
 	flag.StringVar(&inputFile, "i", "", "sets input file")
@@ -16,5 +24,16 @@ func main() {
 	flag.StringVar(&key, "key", "", "sets key (used only for `des` mode)")
 	flag.Parse()
 
-	
+	var cipher cipher.Block
+	if isTriple {
+		cipher, err = des.NewTripleDESCipher([]byte(key))
+	} else {
+		cipher, err = des.NewCipher([]byte(key))
+	}
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := make([]byte, 8)
+	cipher.Encrypt([]byte("pudgeboo"), result)
+	fmt.Println(result)
 }
